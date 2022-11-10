ebff51f545e96a8bc872afbab00fc9bdede4d00c
Ualace Santos <89046095+Ualace36@users.noreply.github.com>
2022-11-09 22:26:39 -0300

    Update Card.jsx
    alteração de arquivo

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card(props) {
   return (
-    <div>
-
-    </div>
+      <div>
+          <p>{ props.values.color}</p>
+          <h1>{ props.values.codigo}</h1>
+      </div>
   )
 }
